6b53a1fb01e7e7ef022017129e5efd5e1b7ddaf1
viswakasi <32349919+viswakasi@users.noreply.github.com>
2026-09-16 16:04:46 -0500

    Re-add API call for watermelon data
    Re-import requests and re-add API call to fetch watermelon data.

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,10 +19,6 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('Fruit
 
 ingredients_list = st.multiselect('Choose up to 5 ingrdient : ', my_dataframe, max_selections=5 )
 
-import requests  
-smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
-st.text(smoothiefroot_response)
-
 if ingredients_list:
 
     ingredients_string = ''
@@ -41,4 +37,8 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
-            
+
+import requests  
+smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+st.text(smoothiefroot_response)
+
