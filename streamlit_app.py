73a221c2922d98008789d39b625188bbeee0b09d
viswakasi <32349919+viswakasi@users.noreply.github.com>
2026-09-16 16:34:17 -0500

    Clean up streamlit_app.py and fix ingredient prompt
    Removed unnecessary 'st' line and fixed typo in multiselect prompt.

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,8 +18,6 @@ session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('Fruit_name'),col('SEARCH_ON'))
 st.dataframe(data=my_dataframe, use_container_width=True)
 st.stop()
-                                                                                                                                           st
-
 ingredients_list = st.multiselect('Choose up to 5 ingrdient : ', my_dataframe, max_selections=5 )
 
 if ingredients_list:
