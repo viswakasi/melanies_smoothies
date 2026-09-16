115c6945205ffb07f3c68a36be28f533069a0632
viswakasi <32349919+viswakasi@users.noreply.github.com>
2026-09-16 16:40:54 -0500

    Integrate pandas for DataFrame conversion and updates
    Added pandas import and converted Snowflake DataFrame to pandas DataFrame. Updated ingredient selection and nutrition information display.

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
+import pandas as pd
 
 # Write directly to the app
 st.title(f":cup_with_straw: Customzie Your Smoothie! :cup_with_straw:")
@@ -16,8 +17,10 @@ st.write('The name of your Smoothie will be: ', name_on_smoothie)
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('Fruit_name'),col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True)
-st.stop()
+pd_df = my_dataframe.to_pandas()
+# st.dataframe(data=my_dataframe, use_container_width=True)
+# st.stop()
+
 ingredients_list = st.multiselect('Choose up to 5 ingrdient : ', my_dataframe, max_selections=5 )
 
 if ingredients_list:
@@ -26,7 +29,11 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-        st.subheader(fruit_chosen + 'Nutrition Information')
+
+        search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chose, ' is ', search_on, '.')
+      
+        st.subheader(fruit_chosen + ' Nutrition Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True )
       
